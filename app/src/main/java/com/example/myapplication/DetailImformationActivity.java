001673228b7f254ff595fef92c30e2c194278b3a
IMGMANITSME <101241626+IMGMANITSME@users.noreply.github.com>
2023-04-26 21:42:45 +0800

    Detail Imformation page #32

--- app/src/main/java/com/example/myapplication/DetailImformationActivity.java
+++ app/src/main/java/com/example/myapplication/DetailImformationActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailImformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail_imformation);
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
