5eef7af509689d009df87850ccead952fb451db1
jeffYang1224 <92380307+qwe8496516@users.noreply.github.com>
2023-05-23 01:28:16 +0800

    #35 Tab功能完成
    #35 Tab功能完成

--- app/src/main/java/com/example/myapplication/OrderActivity.java
+++ app/src/main/java/com/example/myapplication/OrderActivity.java
@@ -1,10 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import Adapter.MyViewAdapter;
+
 public class OrderActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private MyViewAdapter myViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,27 @@ public class OrderActivity extends AppCompatActivity {
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+        myViewAdapter = new MyViewAdapter(this);
+        viewPager2.setAdapter(myViewAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
