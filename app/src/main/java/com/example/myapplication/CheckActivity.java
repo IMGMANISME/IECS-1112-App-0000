c96f6f402ee56df560657f0b1e8c5498f9efa44d
jeffYang1224 <92380307+qwe8496516@users.noreply.github.com>
2023-05-24 13:31:10 +0800

    #38 確認訂單頁面UI實作
    版面配置完成

---
+++ app/src/main/java/com/example/myapplication/CheckActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+    }
+}
